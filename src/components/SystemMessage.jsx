576eef4b752a2b951fb40a7349541eccb6d7f4ee
Umar <umar@volmatica.com>
2023-10-16 20:26:42 +0500

    changed prompts, also added env file.

--- src/components/SystemMessage.jsx
+++ src/components/SystemMessage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import Markdown from 'react-markdown'
 
 function SystemMessage({ message }) {
+    
     return (
         <div className="systemMessage">
             <div className="flex-shrink-0 h-10 w-10 rounded-full bg-[#007BA7] center"></div>
             <div>
                 <div className="systemMessage_box">
-                    <p className="text-sm">{message}</p>
+                    {/* <p className="text-sm">{message}</p> */}
+                    <Markdown className="text-sm">{message}</Markdown>
                 </div>
                 {/* <span className="systemMessage_time">2 min ago</span> */}
             </div>
--- src/hooks/AllData.js
+++ src/hooks/AllData.js
@@ -4,11 +4,22 @@ import { useState } from "react";
 const AllData = () => {
     const [fullChat, setFullChat] = useState([
         { "role": "system", "content": "You are an AI assistant." },
-        { "role": "system", "content": "Your goal is to engage the user by asking 3 questions about general facts, one question at a time." },
-        { "role": "system", "content": "Avoid providing feedback on whether the user's answers are correct or not." },
-        { "role": "system", "content": "After the user answers all three questions, generate a brief summary of the questions and answers." },
-        { "role": "system", "content": "Include information on what the user got right and what was incorrect." },
-        { "role": "system", "content": "Begin the interaction with only a friendly greeting to the user. Don't ask any question, just tell them what we are going to do" }
+        { "role": "system", "content": "Your goal is to gather information about the user's project. Ask the following questions one at a time, each question should be asked separately, but don't ask it yet." },
+        { "role": "system", "content": "Greet the user with a warm greeting and ask if they are ready to answer questions regarding their project." },
+        { "role": "system", "content": "If the user doesn't answer the questions properly, ask that question again before moving on to the next one." },
+        { "role": "system", "content": "If the user tries to ask something or goes off track, bring them back on track following the questionnaire." },
+        {
+            "role": "system",
+            "content": "After the user completes the questions, provide them with a comprehensive summary of their answers in a neatly formatted document. Ensure each key point is presented under its own heading"
+        },
+        { "role": "system", "content": "Remember to answer each question thoughtfully." },
+        { "role": "system", "content": "1. What is the main goal of your project?" },
+        { "role": "system", "content": "2. Briefly describe the nature of your project." },
+        { "role": "system", "content": "3. What is the scope of your project?" },
+        { "role": "system", "content": "4. Is it a personal project, academic, or professional?" },
+        { "role": "system", "content": "5. What is the expected timeline for your project?" },
+        { "role": "system", "content": "6. Have you faced any challenges or obstacles in your project so far?" },
+        { "role": "system", "content": "7. Would you be open to further discussions or assistance or feedback regarding your project?" },
     ]
     );
 
