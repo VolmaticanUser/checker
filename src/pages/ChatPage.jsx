7b24dc440692eb99370ff2f794c58c0990abb137
Umar <umar@volmatica.com>
2023-10-24 23:28:08 +0500

    tweaks

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -66,7 +66,7 @@ function ChatPage() {
     return (
         <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-no-repeat bg-cover text-gray-800 p-10 fancyBackground">
             <div className="flex flex-col flex-grow w-full max-w-xl chatContainer shadow-xl rounded-lg overflow-hidden">
-                <div className="flex flex-col justify-end flex-grow h-0 p-4 overflow-auto" ref={messageWindow}>
+                <div className="flex flex-col flex-grow h-0 p-4 overflow-auto" ref={messageWindow}>
                     {ChatElement}
                 </div>
                 <div className="bg-gray-300 p-4 inputContainer">
